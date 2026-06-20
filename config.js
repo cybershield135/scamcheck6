27c725e5d5dc82253e02a442f37d5ecc460bf1a4
cybershield135 <hytesting56@gmail.com>
2026-06-20 09:34:09 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    GEMINI_API_KEY: 'AQ.Ab8RN6JBn89hulwqc-Ea_IHP_iXNw8gjlUHR9sRkICGfrWxjoQ',
+    API_URL: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent'
+};
+
+export default CONFIG;
